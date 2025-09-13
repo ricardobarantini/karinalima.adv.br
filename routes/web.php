179e2f34206102ed9dc285008697ee6eec78e7c9
Ricardo Barantini <ricardobarantini@gmail.com>
2025-09-13 19:36:29 -0300

    remove all laravel files

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    $whatsappLink = sprintf("https://wa.me/%s?text=%s", "5511965232865", urlencode("Olá. Vi seu site e preciso de um especialista."));
-    return view('index')->with('whatsappLink', $whatsappLink);
-});
