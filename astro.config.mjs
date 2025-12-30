04789091812c21ff7cde8a56984916328d9670ed
Ricardo Barantini <ricardobarantini@gmail.com>
2025-12-30 14:53:08 -0300

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://karinalima.adv.br',
-    base: '/karinalima.adv.br',
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  site: "https://karinalima.adv.br",
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
